203c972f885c0b7821251664465370dc88aa67fb
Daniyel Tkachuk <danilaradyn7357899@gmail.com>
2024-04-23 23:37:49 +0300

    added components: Header, Footer, Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+
+export const App = () => {
   return (
     <div className="App">
 
     </div>
   );
 }
-
-export default App;
