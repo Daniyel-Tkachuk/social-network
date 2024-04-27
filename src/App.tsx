7741aec2e5c1bed605637afa9957d5bdd86e22e9
Daniyel Tkachuk <danilaradyn7357899@gmail.com>
2024-04-27 23:34:09 +0300

    added project skeleton (html)

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,50 @@ import './App.css';
 
 
 export const App = () => {
-  return (
-    <div className="App">
-
-    </div>
-  );
+   return (
+      <div className="app-wrapper">
+         <header className="header">
+            <img src="#" alt="logo"/>
+         </header>
+         <nav className="nav">
+            <div>
+               <a>Profile</a>
+            </div>
+            <div>
+               <a>Messages</a>
+            </div>
+            <div>
+               <a>News</a>
+            </div>
+            <div>
+               <a>Music</a>
+            </div>
+            <div>
+               <a>Settings</a>
+            </div>
+         </nav>
+         <div className="content">
+            <div>
+               <img src="#" alt="banner"/>
+            </div>
+            <div>
+               ava + description
+            </div>
+            <div>
+               My posts
+               <div>
+                  New post
+               </div>
+               <div>
+                  <div>
+                     post-1
+                  </div>
+                  <div>
+                     post-2
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   );
 }
