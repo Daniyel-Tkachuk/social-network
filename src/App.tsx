ef6c9c4fbf8dba344e178a87ab26e7fc1832e9b6
Daniyel Tkachuk <danilaradyn7357899@gmail.com>
2024-05-07 11:20:37 +0300

    added navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
 import {Header} from "./components/header/Header";
 import {Navbar} from "./components/navbar/Navbar";
 import {Profile} from "./components/profile/Profile";
 import {Dialogs} from "./components/dialogs/Dialogs";
 
 
-
-export const App = () => {
+const App = () => {
    return (
-         <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            <div className={"app-wrapper_content"}>
-               <Profile/>
-               {/*<Dialogs/>*/}
-            </div>
+      <div className="app-wrapper">
+         <Header/>
+         <Navbar/>
+         <div className={"app-wrapper_content"}>
+            <Routes>
+               <Route path="/profile" element={<Profile/>}/>
+               <Route path="/dialogs" element={<Dialogs/>}/>
+            </Routes>
          </div>
-   );
+      </div>
+   )
 }
+
+export default App
