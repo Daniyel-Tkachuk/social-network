81e8bc236da460ab7e2f113da2c12c8710366084
Daniyel Tkachuk <danilaradyn7357899@gmail.com>
2024-05-21 18:53:06 +0300

    refactor

--- src/components/profile/postContainer/PostContainer.tsx
+++ src/components/profile/postContainer/PostContainer.tsx
@@ -9,6 +9,13 @@ type Props = {
 export const PostContainer: FC<Props> = (props) => {
    const {} = props;
 
+   const postsData = [
+      {id: 1, postMessage: "post-1", likeCount: 2},
+      {id: 2, postMessage: "post-2", likeCount: 4},
+      {id: 3, postMessage: "post-3", likeCount: 10},
+      {id: 4, postMessage: "post-4", likeCount: 8},
+   ]
+
    return (
       <div>
          <h3>My posts</h3>
