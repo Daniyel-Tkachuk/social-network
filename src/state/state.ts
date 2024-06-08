e37d4e3723aac4798d9e3830bbebc2bbe4c17655
Daniyel Tkachuk <danilaradyn7357899@gmail.com>
2024-06-08 17:36:29 +0300

    added general state.ts

---
+++ src/state/state.ts
@@ -0,0 +1,50 @@
+export type PostType = {
+   id: number
+   postMessage: string
+   likeCount: number
+}
+export type DialogsType = {
+   id: number
+   name: string
+}
+export type MessagesType = {
+   id: number
+   message: string
+}
+export type ProfilePageType = {
+   posts: PostType[]
+}
+export type MessagesPageType = {
+   dialogs: DialogsType[]
+   messages: MessagesType[]
+}
+
+export type StateType = {
+   profilePageData: ProfilePageType
+   messagesPageData: MessagesPageType
+}
+
+export const state: StateType = {
+   profilePageData: {
+      posts: [
+         {id: 1, postMessage: "post-1", likeCount: 2},
+         {id: 2, postMessage: "post-2", likeCount: 4},
+         {id: 3, postMessage: "post-3", likeCount: 10},
+         {id: 4, postMessage: "post-4", likeCount: 8},
+      ],
+   },
+   messagesPageData: {
+      dialogs: [
+         {id: 1, name: "Daniyel"},
+         {id: 2, name: "Viktoriya"},
+         {id: 3, name: "Zakhar"},
+         {id: 4, name: "Andrey"},
+         {id: 5, name: "Tereza"},
+      ],
+      messages: [
+         {id: 1, message: "Hi"},
+         {id: 2, message: "How is your it-kamasutra?"},
+         {id: 3, message: "Yo! How are you ?"},
+      ]
+   }
+}
